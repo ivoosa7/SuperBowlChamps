54cf013fefd8da1e3c875b18f0fa3b107c147da1
Ivo Araujo <ivo.souza-23@outlook.com>
2025-02-11 12:46:30 -0300

    Função get feita no arquivo.ts no componente sbchampionslist

--- src/app/sbchampions-list/sbchampions-list.component.ts
+++ src/app/sbchampions-list/sbchampions-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SbChamps } from '../SbChamps';
+import { SuperBowlService } from '../super-bowl.service';
 
 @Component({
   selector: 'app-sbchampions-list',
   templateUrl: './sbchampions-list.component.html',
   styleUrl: './sbchampions-list.component.css'
 })
-export class SBChampionsListComponent {
+export class SBChampionsListComponent implements OnInit{
+  sbchampions : SbChamps [] = [];
+  constructor(private service:SuperBowlService){}
 
+  ngOnInit(): void {
+      this.loadChampions();
+  }
+
+  loadChampions(){
+    this.service.getChamps().subscribe({
+      next: data => this.sbchampions = data
+    })
+  };
 }
