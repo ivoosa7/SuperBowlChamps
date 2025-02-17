f1e385608646a28b973c44331f793ea52311c67e
Ivo Araujo <ivo.souza-23@outlook.com>
2025-02-17 08:16:04 -0300

    A lista de campeões do sb esta funcionando, a tabela está na estilização correta, foi feito os path em app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SBChampionsListComponent } from './sbchampions-list/sbchampions-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  {path: 'superbowlchampions', component: SBChampionsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
