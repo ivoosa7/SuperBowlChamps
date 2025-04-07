8fdba5ecd7c037883258479f8ee2fa2753d67743
Ivo Araujo <ivo.souza-23@outlook.com>
2025-04-07 11:44:15 -0300

    Colocando mais 2 opções na lista de campeões, Alterar e Deletar

--- src/app/sbchampions-list/sbchampions-list.component.ts
+++ src/app/sbchampions-list/sbchampions-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SbChamps } from '../SbChamps';
 import { SuperBowlService } from '../super-bowl.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sbchampions-list',
@@ -9,17 +10,28 @@ import { SuperBowlService } from '../super-bowl.service';
 })
 export class SBChampionsListComponent implements OnInit{
   sbchampions : SbChamps [] = [];
-  constructor(private service:SuperBowlService){}
+  constructor(private service:SuperBowlService, private rota:Router){}
 
   ngOnInit(): void {
       this.loadChampions();
   }
 
+  //Para carregar a função de pegar todos os itens
   loadChampions(){
     this.service.getChamps().subscribe({
       next: data => this.sbchampions = data
     })
   };
 
-  
+  //Alterar sb champs
+  alterandosbChamps(id: Number){
+    this.rota.navigate(['/newsuperbowlchampions', id]);
+  }
+
+  //Deletar Função
+  deletarSbChamps(sb: SbChamps){
+    this.service.deleteWrongChamps(sb).subscribe({
+      next: () => this.loadChampions()
+    });
+  }
 }
